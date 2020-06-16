f7741f70d3560df7530bac7dd127d8465c3377c0
skboy <364971779@qq.com>
2020-06-16 20:07:33 +0800

    cart

---
+++ models/cart-item.js
@@ -0,0 +1,15 @@
+class CartItem {
+    skuId = null
+    count = 0;
+    sku = null
+    checked = true
+    constructor(sku,count) {
+        this.skuId=sku.id
+        this.sku=sku
+        this.count=count
+    }
+
+}
+export {
+    CartItem
+}
